9bd516e3093e6d1a9dc32951972238c28f30f190
russotx <russotx@gmail.com>
2017-04-29 00:23:47 -0500

    bug fixes

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,10 @@
-const burgerDB = require('connection.js');
+const burgerDB = require('./connection.js');
 
 const orm = {
   
   selectALL : function(table,callback){
-    const query = "SELECT * FROM ?";
-    burgerDB.query(query,[table],function(err,result){
+    const query = "SELECT * FROM "+table+";";
+    burgerDB.query(query,function(err,result){
       if (err) throw err;
       callback(result);
     });
